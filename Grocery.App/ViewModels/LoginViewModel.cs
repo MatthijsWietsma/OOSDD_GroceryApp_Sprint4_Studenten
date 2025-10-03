62ec3e01e9e20835df984fac2f23a99b8c5d2a55
co2mott <matthijswietsma@gmail.com>
2025-10-03 11:04:00 +0200

    UC13 is uitgewerkt

--- Grocery.App/ViewModels/LoginViewModel.cs
+++ Grocery.App/ViewModels/LoginViewModel.cs
@@ -1,5 +1,4 @@
-﻿
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
@@ -10,6 +9,7 @@ namespace Grocery.App.ViewModels
     {
         private readonly IAuthService _authService;
         private readonly GlobalViewModel _global;
+        private readonly IClientService _clientService;
 
         [ObservableProperty]
         private string email = "user3@mail.com";
@@ -20,10 +20,11 @@ namespace Grocery.App.ViewModels
         [ObservableProperty]
         private string loginMessage;
 
-        public LoginViewModel(IAuthService authService, GlobalViewModel global)
-        { //_authService = App.Services.GetServices<IAuthService>().FirstOrDefault();
+        public LoginViewModel(IAuthService authService, GlobalViewModel global, IClientService clientService)
+        {
             _authService = authService;
             _global = global;
+            _clientService = clientService;
         }
 
         [RelayCommand]
@@ -33,7 +34,10 @@ namespace Grocery.App.ViewModels
             if (authenticatedClient != null)
             {
                 LoginMessage = $"Welkom {authenticatedClient.Name}!";
+
                 _global.Client = authenticatedClient;
+                _clientService.CurrentClient = authenticatedClient;
+
                 Application.Current.MainPage = new AppShell();
             }
             else
